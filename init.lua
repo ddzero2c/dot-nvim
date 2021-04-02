0d6147c5c6765acd0b695c42d813e869a96bf4e2
Ryder <ddzero2c@gmail.com>
2021-04-03 01:36:59 +0800

    fix: whichkey long delay

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 vim.cmd('source ~/.config/nvim/plugins.vim')
 vim.cmd('source ~/.config/nvim/rc.vim')
-vim.cmd('source ~/.config/nvim/whichkey.vim')
 vim.cmd('source ~/.config/nvim/coc.vim')
 
 require('colorscheme')
 require('statusline')
 require('treesitter')
 require('indentline')
+vim.cmd('source ~/.config/nvim/whichkey.vim')
