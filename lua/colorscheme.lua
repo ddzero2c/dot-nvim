2191cfe95fe03d374fb9360a22f0c7a09e77bdd9
Ryder <ddzero2c@gmail.com>
2021-04-03 03:55:46 +0800

    chore: move color to anothor repo

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,7 +1,1 @@
 vim.cmd('colorscheme lumiere')
-
-vim.cmd('hi! link CocErrorSign ErrorMsg')
-vim.cmd('hi! link CocWarningSign WarningMsg')
-vim.cmd('hi! link CocInfoSign InfoMsg')
-vim.cmd('hi! link CocHintSign HintMsg')
-vim.cmd('hi! link CocHighlightText StatusLine')
