43978d05b0f5b82b7543a85a5f2122af9e37fa8b
Ryder <ddzero2c@gmail.com>
2021-10-02 16:12:20 +0800

    chore: adjust dap

--- lua/settings/lsp-gopls.lua
+++ lua/settings/lsp-gopls.lua
@@ -21,10 +21,8 @@ require("lspconfig").gopls.setup(lsp.ensure_capabilities({
 require("go").setup({
 	goimport = "gopls",
 	gofmt = "gopls",
-	dap_debug = true,
-	dap_debug_gui = true,
+	dap_debug = false,
+	dap_debug_gui = false,
 	dap_debug_vt = false,
 })
 vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
-vim.fn.sign_define("DapBreakpoint", { text = "•", texthl = "", linehl = "", numhl = "" })
-vim.fn.sign_define("DapStopped", { text = "→", texthl = "", linehl = "Cursor", numhl = "" })
--- lua/settings/nvim-dap.lua
+++ lua/settings/nvim-dap.lua
@@ -1,22 +1,34 @@
 local dap = require("dap")
 dap.adapters.go = function(callback, config)
+	local stdout = vim.loop.new_pipe(false)
 	local handle
 	local pid_or_err
 	local port = 38697
-	handle, pid_or_err = vim.loop.spawn("dlv", {
+	local opts = {
+		stdio = { nil, stdout },
 		args = { "dap", "-l", "127.0.0.1:" .. port },
 		detached = true,
-	}, function(code)
+	}
+	handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+		stdout:close()
 		handle:close()
-		print("Delve exited with exit code: " .. code)
+		if code ~= 0 then
+			print("dlv exited with code", code)
+		end
 	end)
-	-- Wait 100ms for delve to start
+	assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+	stdout:read_start(function(err, chunk)
+		assert(not err, err)
+		if chunk then
+			vim.schedule(function()
+				require("dap.repl").append(chunk)
+			end)
+		end
+	end)
+	-- Wait for delve to start
 	vim.defer_fn(function()
-		--dap.repl.open()
 		callback({ type = "server", host = "127.0.0.1", port = port })
 	end, 100)
-
-	--callback({type = "server", host = "127.0.0.1", port = port})
 end
 -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
 dap.configurations.go = {
@@ -31,7 +43,15 @@ dap.configurations.go = {
 		name = "Debug test", -- configuration for debugging test files
 		request = "launch",
 		mode = "test",
-		program = ".",
+		program = "${file}",
+	},
+	-- works with go.mod packages and sub packages
+	{
+		type = "go",
+		name = "Debug test (go.mod)",
+		request = "launch",
+		mode = "test",
+		program = "./${relativeFileDirname}",
 	},
 }
 
@@ -39,9 +59,9 @@ vim.fn.sign_define("DapBreakpoint", { text = "•", texthl = "", linehl = "", nu
 vim.fn.sign_define("DapStopped", { text = "→", texthl = "", linehl = "Cursor", numhl = "" })
 
 vim.cmd([[
-nnoremap <silent> <F4> :lua require'dap'.stop()<CR>
+nnoremap <silent> <F4> :lua require'dap'.close()<CR>:lua require'dapui'.close()<CR>:set mouse=<CR>
 nnoremap <silent> <F5> :lua require'dap'.continue()<CR>
-nnoremap <silent> <F8> :lua require("dapui").toggle()<CR>
+nnoremap <silent> <F8> :lua require("dapui").open()<CR>:set mouse=n<CR>
 nnoremap <silent> <F9> :lua require'dap'.toggle_breakpoint()<CR>
 nnoremap <silent> <F10> :lua require'dap'.step_over()<CR>
 nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>
@@ -49,37 +69,41 @@ nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>
 ]])
 
 require("dapui").setup({
-	icons = {
-		expanded = "▼",
-		collapsed = "▶",
-	},
+	icons = { expanded = "▾", collapsed = "▸" },
 	mappings = {
 		-- Use a table to apply multiple mappings
 		expand = { "<CR>", "<2-LeftMouse>" },
 		open = "o",
 		remove = "d",
 		edit = "e",
+		repl = "r",
 	},
 	sidebar = {
-		open_on_start = true,
+		-- You can change the order of elements in the sidebar
 		elements = {
-			-- You can change the order of elements in the sidebar
-			"scopes",
-			"stacks",
+			-- Provide as ID strings or tables with "id" and "size" keys
+			{
+				id = "scopes",
+				size = 0.25, -- Can be float or integer > 1
+			},
+			{ id = "breakpoints", size = 0.25 },
+			{ id = "stacks", size = 0.25 },
+			{ id = "watches", size = 00.25 },
 		},
-		width = 40,
-		position = "right", -- Can be "left" or "right"
+		size = 40,
+		position = "right", -- Can be "left", "right", "top", "bottom"
 	},
 	tray = {
-		open_on_start = true,
-		elements = {
-			"watches",
-		},
-		height = 5,
-		position = "bottom", -- Can be "bottom" or "top"
+		elements = { "repl" },
+		size = 10,
+		position = "bottom", -- Can be "left", "right", "top", "bottom"
 	},
 	floating = {
 		max_height = nil, -- These can be integers or a float between 0 and 1.
 		max_width = nil, -- Floats will be treated as percentage of your screen.
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
 	},
+	windows = { indent = 1 },
 })
