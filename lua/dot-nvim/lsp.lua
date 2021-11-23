249bf3d8e671ca1d3486918f1de63489dcbed568
Ryder <ddzero2c@gmail.com>
2021-11-23 17:09:20 +0800

    fix: null_ls api changes

--- lua/dot-nvim/lsp.lua
+++ lua/dot-nvim/lsp.lua
@@ -167,61 +167,26 @@ vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').
 -- Typescript & Javascript LSP --
 -- npm install -g typescript typescript-language-server
 -- enable null-ls integration (optional)
-require("null-ls").config({})
-lsp["null-ls"].setup({})
 lsp.tsserver.setup({
-	-- capabilities = capabilities,
+	-- Needed for inlayHints. Merge this table with your settings or copy
+	-- it from the source if you want to add your own init_options.
+	init_options = require("nvim-lsp-ts-utils").init_options,
+	--
 	on_attach = function(client, bufnr)
 		on_attach(client, bufnr)
-		-- disable tsserver formatting if you plan on formatting via null-ls
 		client.resolved_capabilities.document_formatting = false
 		client.resolved_capabilities.document_range_formatting = false
-		-- define an alias
 		vim.cmd("command -buffer Formatting lua vim.lsp.buf.formatting()")
 		vim.cmd("command -buffer FormattingSync lua vim.lsp.buf.formatting_sync()")
-
 		-- format on save
 		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-
 		local ts_utils = require("nvim-lsp-ts-utils")
 
 		-- defaults
 		ts_utils.setup({
-			debug = false,
-			disable_commands = false,
-			enable_import_on_completion = true,
-
-			-- import all
-			import_all_timeout = 5000, -- ms
-			import_all_priorities = {
-				buffers = 4, -- loaded buffer names
-				buffer_content = 3, -- loaded buffer content
-				local_files = 2, -- git files or files with relative path markers
-				same_file = 1, -- add to existing import statement
-			},
-			import_all_scan_buffers = 100,
-			import_all_select_source = false,
-
-			-- eslint
-			eslint_enable_code_actions = true,
-			eslint_enable_disable_comments = true,
-			eslint_bin = "eslint",
-			eslint_enable_diagnostics = true,
-			eslint_opts = {},
-
-			-- formatting
-			enable_formatting = true,
-			formatter = "prettier",
-			formatter_opts = {},
-
-			-- update imports on file move
+			filter_out_diagnostics_by_severity = { "hint" },
 			update_imports_on_move = true,
 			require_confirmation_on_move = true,
-			watch_dir = nil,
-
-			-- filter diagnostics
-			filter_out_diagnostics_by_severity = { "hint" },
-			filter_out_diagnostics_by_code = {},
 		})
 
 		-- required to fix code action ranges and filter diagnostics
@@ -229,11 +194,21 @@ lsp.tsserver.setup({
 
 		-- no default maps, so you may want to define some here
 		local opts = { silent = true }
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "gO", ":TSLspOrganize<CR>", opts)
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "gR", ":TSLspRenameFile<CR>", opts)
-		vim.api.nvim_buf_set_keymap(bufnr, "n", "gI", ":TSLspImportAll<CR>", opts)
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
+		-- vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
+		-- vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
 	end,
 })
+local null_ls = require("null-ls")
+null_ls.config({
+	sources = {
+		null_ls.builtins.diagnostics.eslint, -- eslint or eslint_d
+		null_ls.builtins.code_actions.eslint, -- eslint or eslint_d
+		null_ls.builtins.formatting.prettier, -- prettier, eslint, eslint_d, or prettierd
+	},
+})
+
+require("lspconfig")["null-ls"].setup({})
 
 -- Python LSP --
 lsp.pyright.setup({
